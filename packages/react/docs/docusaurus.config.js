20989749818823493bb366694ad3d6f57e947f81
Dongsheng <1197160272@qq.com>
2022-04-01 15:57:56 +0800

    change react doc footer info

--- packages/react/docs/docusaurus.config.js
+++ packages/react/docs/docusaurus.config.js
@@ -128,7 +128,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Casual UI.`,
       },
       prism: {
         theme: lightCodeTheme,
