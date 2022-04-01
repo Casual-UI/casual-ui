d4d6b0bb73037383f2b5f814f6762b4dfe47e37e
Dongsheng <1197160272@qq.com>
2022-04-01 12:29:20 +0800

    fix: vue dev docs clear temp file path

--- packages/vue/docs/cli/clearTemp.js
+++ packages/vue/docs/cli/clearTemp.js
@@ -3,7 +3,7 @@ const path = require('path')
 const CWD_PATH = process.cwd()
 
 fs.rm(
-  path.resolve(CWD_PATH, './docs/.vuepress/.temp/.casual'),
+  path.resolve(CWD_PATH, '.vuepress/.temp/.casual'),
   {
     recursive: true,
     force: true,
