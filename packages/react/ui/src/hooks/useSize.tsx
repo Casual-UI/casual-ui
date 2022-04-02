e9fdea1a6abc13faa0f8441ab50857d00c23ed18
Dongsheng <1197160272@qq.com>
2022-04-02 18:21:14 +0800

    docs: React DatePicker sizes doc

--- packages/react/ui/src/hooks/useSize.tsx
+++ packages/react/ui/src/hooks/useSize.tsx
@@ -4,13 +4,10 @@ const defaultSize: CSize = 'md'
 
 const CSizeContext = createContext<CSize>(defaultSize)
 
-const useSize = (size: CSize = defaultSize) => {
+const useSize = (size?: CSize) => {
   const contextSize = useContext(CSizeContext)
 
-  return useMemo(
-    () => (size ? size : contextSize ? contextSize : 'md'),
-    [size, contextSize]
-  )
+  return useMemo(() => (size ? size : contextSize), [size, contextSize])
 }
 export default useSize
 
