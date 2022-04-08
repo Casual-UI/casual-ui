a5051925d703b0c6cbc99e6bfb181aad907d8af3
Dongsheng Zhao <1197160272@qq.com>
2022-04-08 21:20:00 +0800

    docs-vue: add Algolia docs search

--- packages/vue/docs/.vuepress/config.ts
+++ packages/vue/docs/.vuepress/config.ts
@@ -3,6 +3,7 @@ import type { DefaultThemeOptions } from 'vuepress'
 import { path } from '@vuepress/utils'
 import { UserConfig } from 'vite'
 import casualCode from 'vuepress-plugin-casual-code'
+import docSearch from '@vuepress/plugin-docsearch'
 
 const iconsRoutes = [
   '/icons-preview/material/',
@@ -24,14 +25,66 @@ export default defineUserConfig<
     viteOptions: UserConfig
   }
 >({
-  port: 8090,
   head: [['link', { rel: 'icon', href: '/logo.svg' }]],
   lang: 'zh-CN',
   title: 'Casual UI - Vue',
   description: '一个随意的组件库',
   theme: '@vuepress/theme-default',
   clientAppEnhanceFiles: [path.resolve(__dirname, './clientAppEnhance.ts')],
-  plugins: [casualCode],
+  plugins: [
+    casualCode, 
+    [
+      docSearch, 
+      {  
+        appId: 'GC34OK1KSK',
+        apiKey: 'e29418351aacb050d006d9e76bdabe16',
+        indexName: 'casual-ui-vue',
+        placeholder: '搜索文档',
+        translations: {
+          button: {
+            buttonText: '搜索',
+            buttonAriaLabel: '搜索'
+          },
+          modal: {
+            searchBox: {
+              resetButtonTitle: '重置查询',
+              resetButtonAriaLabel: '重置查询',
+              cancelButtonText: '取消',
+              cancelButtonAriaLabel: '取消',
+            },
+            startScreen: {
+              recentSearchesTitle: '最近',
+              noRecentSearchesText: '无最近搜索',
+              saveRecentSearchButtonTitle: '保存当前搜索',
+              removeRecentSearchButtonTitle: '从历史中删除当前搜索',
+              favoriteSearchesTitle: '最爱的搜索',
+              removeFavoriteSearchButtonTitle: '从最爱中移除',
+            },
+            errorScreen: {
+              titleText: '无法获取搜索结果',
+              helpText: '请检查网络连接',
+            },
+            footer: {
+              selectText: '请选择',
+              selectKeyAriaLabel: '输入关键字',
+              navigateText: '导航',
+              navigateUpKeyAriaLabel: '向上',
+              navigateDownKeyAriaLabel: '向下',
+              closeText: '关闭',
+              closeKeyAriaLabel: '关闭',
+              searchByText: '根据文字搜索',
+            },
+            noResultsScreen: {
+              noResultsText: '没有找到结果',
+              suggestedQueryText: '您可能想搜索',
+              reportMissingResultsText: '没有找到结果？请报告缺失',
+              reportMissingResultsLinkText: '报告缺失',
+            },
+          },
+        }
+      }
+    ]
+  ],
   clientAppRootComponentFiles: [
     path.resolve(__dirname, './components/UseCNotification.vue'),
   ],
