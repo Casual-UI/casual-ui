bc18ee6e24be3d45bd0fdd0cf6d5f357a3234fd9
Dongsheng Zhao <1197160272@qq.com>
2022-07-07 16:05:12 +0800

    refactor: move casual config to theme directory

---
+++ packages/svelte/docs/src/theme/casual.config.ts
@@ -0,0 +1,37 @@
+export default {
+  github: 'https://github.com/Blackman99/casual-ui',
+  features: [
+    {
+      label: 'Written in TS',
+      desc: 'Friendly typescript support',
+    },
+    {
+      label: 'Small size',
+      desc: 'No extra large third-party lib',
+    },
+    {
+      label: 'Support dark mode',
+      desc: 'All the components has a dark mode ui style',
+    },
+    {
+      label: 'Strong and flexiable form',
+      desc: 'Provide a form interface for you to fully customize your app',
+    },
+    // NOTICE: support for maximum 6 features
+  ],
+  navbar: [
+    {
+      label: '指南',
+      children: [
+        {
+          label: '安装',
+          to: '/guide/install/',
+        },
+        {
+          label: '主题定制',
+          to: '/guide/theme-customize/',
+        },
+      ],
+    },
+  ],
+}
