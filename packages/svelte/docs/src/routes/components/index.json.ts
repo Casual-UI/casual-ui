d2400daa206d31c672b15511ea568373b8d7b6a4
Dongsheng <1197160272@qq.com>
2022-07-16 21:55:06 +0800

    styles: add build script

--- packages/svelte/docs/src/routes/components/index.json.ts
+++ packages/svelte/docs/src/routes/components/index.json.ts
@@ -1,13 +1,18 @@
 import type { RequestHandler } from '@sveltejs/kit'
 
-const components = import.meta.globEager('./**/*.svelte')
+const components = import.meta.glob('./**/*.svelte', { eager: true })
 
+// parse the sidebar info
 const sidebar = Object.entries(components)
   .filter(
     ([k]) =>
+      // exclude the layouts
       k.indexOf('__layout.svelte') === -1 &&
+      // exclude the index pages
       k !== './index.svelte' &&
+      // exclude the demo components
       k.indexOf('_demos') === -1 &&
+      // exclude the docs components
       k.indexOf('_components') === -1
   )
   .map(([k, v]: any) => ({
