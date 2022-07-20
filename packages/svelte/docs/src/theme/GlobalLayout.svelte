5be0c287f83806b0cda93d1bc1ce401186087209
Dongsheng Zhao <1197160272@qq.com>
2022-07-20 10:54:59 +0800

    docs: change svelte doc global font family

--- packages/svelte/docs/src/theme/GlobalLayout.svelte
+++ packages/svelte/docs/src/theme/GlobalLayout.svelte
@@ -1,11 +1,4 @@
-<div
-  flex-col
-  flex
-  font-mono
-  text-t1
-  pt-18
-  style="min-height: calc(100vh - 4.5rem);"
->
+<div flex-col flex text-t1 pt-18 style="min-height: calc(100vh - 4.5rem);">
   <slot name="header" />
   <div flex-grow>
     <slot />
