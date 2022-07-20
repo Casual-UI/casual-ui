31277935e9478d2d8285bdabe5c6bdbad34702c9
Dongsheng Zhao <1197160272@qq.com>
2022-07-20 10:59:35 +0800

    docs: svelte button customize loading instance demo

---
+++ packages/svelte/docs/src/routes/components/button/_demos/customize-loading-instance.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import CButton from 'casual-ui-svelte/src/components/CButton.svelte'
+  import CLoadingBar from 'casual-ui-svelte/src/components/loadings/CLoadingBar.svelte'
+  import CLoadingHourglass from 'casual-ui-svelte/src/components/loadings/CLoadingHourglass.svelte'
+</script>
+
+<CButton loading label="Loading...">
+  <CLoadingHourglass slot="loading" />
+</CButton>
+<CButton loading label="Loading..." theme="secondary">
+  <CLoadingBar slot="loading" />
+</CButton>
