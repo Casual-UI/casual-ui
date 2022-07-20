745a11ebece1a7f71f2843be31f0516d17a5e7ce
Dongsheng Zhao <1197160272@qq.com>
2022-07-20 17:05:22 +0800

    docs: add manual expansion control demo

---
+++ packages/svelte/src/routes/components/expansion/_demos/manual-control.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import CButton from '$lib/components/CButton.svelte'
+  import { CExpansion } from 'casual-ui-svelte'
+
+  let expanded = false
+</script>
+
+<CButton
+  rounded
+  mb-4
+  label="Click Me"
+  on:click={() => (expanded = !expanded)}
+/>
+<CExpansion title="Click button above toggle expand status" bind:expanded>
+  <p>Some content</p>
+</CExpansion>
