0ce360adba4b1fb7b997191b0c0924fa478636c2
Dongsheng <1197160272@qq.com>
2022-07-24 15:16:19 +0800

    feat-svelte: add Notification

---
+++ packages/svelte/src/routes/components/notification/_demos/basic-usage.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import CButton from '$lib/components/CButton.svelte'
+  import { openNotification } from 'casual-ui-svelte'
+</script>
+
+<CButton
+  label="Click to open notification"
+  on:click={() =>
+    openNotification({
+      timeout: 0,
+      title: 'Hi, there',
+      message: "You've got a new notification",
+    })}
+/>
