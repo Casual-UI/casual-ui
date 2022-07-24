675b4d8c838b30ecf84743706e614e30072947d8
Dongsheng <1197160272@qq.com>
2022-07-24 15:22:17 +0800

    docs-svelte: notification themes demo

---
+++ packages/svelte/src/routes/components/notification/_demos/themes.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { CButton, openNotification } from 'casual-ui-svelte'
+
+  function openWithTheme(
+    theme: 'primary' | 'secondary' | 'warning' | 'negative' = 'primary'
+  ) {
+    openNotification({
+      title: 'Hi, there',
+      message: "You've got a new notification",
+      theme,
+    })
+  }
+</script>
+
+<CButton label="Primary (default)" on:click={() => openWithTheme()} />
+
+<CButton
+  label="Secondary"
+  theme="secondary"
+  on:click={() => openWithTheme('secondary')}
+/>
+<CButton
+  label="Warning"
+  theme="warning"
+  on:click={() => openWithTheme('warning')}
+/>
+<CButton
+  label="Negative"
+  theme="negative"
+  on:click={() => openWithTheme('negative')}
+/>
