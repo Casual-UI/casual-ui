0e953ff5b148aea175118d2de3c4147b7d28d3dd
Dongsheng Zhao <1197160272@qq.com>
2022-07-27 18:42:49 +0800

    docs-svelte: fix sidebar transition cuase page toggle not working as expected

--- packages/svelte/src/theme/Sidebar.svelte
+++ packages/svelte/src/theme/Sidebar.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div list-none leading-8 m-0 p-0 inline-block pr-8>
-  {#each links as { label, to }}
+  {#each links as { label, to } (to)}
     <div flex items-center relative>
       {#if activeLink.includes(to)}
         <div
