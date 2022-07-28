d68a22fd3d93af562b28af688a38db2ab6ec4b95
Dongsheng Zhao <1197160272@qq.com>
2022-07-28 11:06:57 +0800

    docs-svelte: copy demo code btn transition

--- packages/svelte/src/theme/CopyBtn.svelte
+++ packages/svelte/src/theme/CopyBtn.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { CButton } from '$lib'
   import { attributeAtom } from '$lib/utils/attributeAtom'
+  import { cubicInOut } from 'svelte/easing'
+  import { slide } from 'svelte/transition'
 
   export let code = ''
 
@@ -18,12 +20,24 @@
 <div relative {...attributeAtom($$restProps)}>
   <slot />
   <div absolute top-2 right-2>
-    <CButton icon style="font-size: 14px;" on:click={onCopy} outlined>
-      {#if copyDone}
-        <div i-material-symbols-done />
-      {:else}
-        <div i-material-symbols-content-copy />
-      {/if}
-    </CButton>
+    {#if copyDone}
+      <div transition:slide={{ duration: 300, easing: cubicInOut }}>
+        <CButton
+          theme="secondary"
+          icon
+          style="font-size: 14px;"
+          on:click={onCopy}
+          outlined
+        >
+          <div i-material-symbols-done />
+        </CButton>
+      </div>
+    {:else}
+      <div transition:slide={{ duration: 300, easing: cubicInOut }}>
+        <CButton icon style="font-size: 14px;" on:click={onCopy} outlined>
+          <div i-material-symbols-content-copy />
+        </CButton>
+      </div>
+    {/if}
   </div>
 </div>
