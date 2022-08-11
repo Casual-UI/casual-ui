d1831d59ffe1c92a9d5b06b3793de465d4491558
Dongsheng Zhao <1197160272@qq.com>
2022-08-11 13:58:50 +0800

    docs-react: drawer English docs

--- packages/react/ui/src/components/popup/CDrawer.tsx
+++ packages/react/ui/src/components/popup/CDrawer.tsx
@@ -6,52 +6,62 @@ type Position = 'left' | 'top' | 'right' | 'bottom'
 
 interface CDrawerProps {
   /**
-   * 抽屉的展示状态
+   * The shown status of drawer.
+   * @zh 抽屉的展示状态
    */
   show?: boolean
 
   /**
-   * 抽屉展示状态变化触发
+   * Emit when the shown status change.
+   * @zh 抽屉展示状态变化触发
    */
   onShowChange?: (newShow: boolean) => void
 
   /**
-   * 抽屉的弹出位置
+   * The drawer position.
+   * @zh 抽屉的弹出位置
    */
   position?: Position
 
   /**
-   * 抽屉标题
+   * The title of drawer.
+   * @zh 抽屉标题
    */
   title?: string
 
   /**
-   * 抽屉宽度，该属性仅在左/右位置时可用
+   * The width of drawer. This prop can only take effect when the position is left or right.
+   * @zh 抽屉宽度，该属性仅在左/右位置时可用
    */
   width?: string
 
   /**
+   * The height of drawer. This prop can only take effect when the position is top or bottom.
    * 抽屉高度，该属性仅在上/下位置时可用
    */
   bodyHeight?: string
 
   /**
-   * 是否点击遮罩收起抽屉
+   * Determine whether to close the drawer when clicked on the backdrop.
+   * @zh 是否点击遮罩收起抽屉
    */
   closeOnClickBackdrop?: boolean
 
   /**
-   * 自定义标题
+   * Customize the title content.
+   * @zh 自定义标题
    */
   customTitle?: CSlot
 
   /**
-   * 自定义关闭图标
+   * Customize the close icon.
+   * @zh 自定义关闭图标
    */
   customCloseIcon?: CSlot
 
   /**
-   * 抽屉内容
+   * The content.
+   * @zh 抽屉内容
    */
   children?: CSlot
 }
