4e7e423ea9f81ec28e17a635d7165bf330783312
Dongsheng Zhao <1197160272@qq.com>
2022-08-11 11:22:08 +0800

    docs-react: add react icon

---
+++ packages/react/docs/src/theme/components/ReactIcon.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function TeenyiconsReactOutline(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="1em"
+      height="1em"
+      viewBox="0 0 15 15"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+      >
+        <path d="M14.5 7.584c0 1.657-3.134 3-7 3s-7-1.343-7-3s3.134-3 7-3s7 1.343 7 3Z"></path>
+        <path d="M4.166 13.739c1.457.79 4.13-1.327 5.972-4.726c1.841-3.4 2.153-6.795.696-7.584c-1.457-.79-4.13 1.327-5.972 4.726c-1.841 3.4-2.153 6.795-.696 7.584Z"></path>
+        <path d="M10.834 13.739c-1.457.79-4.13-1.327-5.972-4.726c-1.841-3.4-2.153-6.795-.696-7.584c1.457-.79 4.13 1.327 5.972 4.726c1.841 3.4 2.153 6.795.696 7.584Z"></path>
+        <path d="M6.5 7.584a1 1 0 1 0 2 0a1 1 0 0 0-2 0Z"></path>
+      </g>
+    </svg>
+  )
+}
