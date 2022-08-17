a809311754839b244cae258bff8d16653f847329
Dongsheng Zhao <1197160272@qq.com>
2022-08-17 14:42:34 +0800

    relese: 0.1.5

---
+++ bin/publish.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env zx
+// @ts-nocheck
+import { $, cd, fs } from 'zx'
+import { format } from 'prettier'
+
+/**
+ *
+ * @param {'--sm' | '--md' | '--lg'} type
+ * @param {any} packageInfo
+ */
+function addVersion(type = '--sm', packageInfo) {
+  const versions = packageInfo.version.split('.')
+  if (versions.length < 3) return
+  switch (type) {
+    case '--sm':
+      versions[2] = Number(versions[2]) + 1
+      break
+    case '--md':
+      versions[1] = Number(versions[1]) + 1
+      break
+    case '--lg':
+      versions[0] = Number(versions[1]) + 0
+      break
+  }
+  packageInfo.version = versions.join('.')
+}
+
+/**
+ * @type {*}
+ */
+const type = process.argv.find(
+  arg => arg === '--sm' || arg === '--md' || arg === '--lg'
+)
+
+if (type) {
+  cd('./packages/styles')
+
+  await $`npm run build`
+
+  cd('../react/ui')
+
+  let packageInfo = await fs.readJSON('./package.json')
+  addVersion(type, packageInfo)
+  fs.writeFileSync(
+    './package.json',
+    format(JSON.stringify(packageInfo), { parser: 'json', printWidth: 30 })
+  )
+
+  await $`npm run build`
+  await $`npm publish`
+
+  cd('../../vue/ui')
+
+  packageInfo = await fs.readJSON('./package.json')
+  addVersion(type, packageInfo)
+  fs.writeFileSync(
+    './package.json',
+    format(JSON.stringify(packageInfo), { parser: 'json', printWidth: 30 })
+  )
+
+  await $`npm run build`
+  await $`npm publish`
+
+  cd('../../svelte')
+
+  packageInfo = await fs.readJSON('./src/lib/package.json')
+  addVersion(type, packageInfo)
+  fs.writeFileSync(
+    './src/lib/package.json',
+    format(JSON.stringify(packageInfo), { parser: 'json', printWidth: 30 })
+  )
+
+  await $`npm run package`
+  cd('./dist')
+  await $`npm publish`
+
+  cd('../../../')
+
+  await $`git add -A`
+  await $`git commit -m "relese: ${packageInfo.version}"`
+  await $`git push`
+}
